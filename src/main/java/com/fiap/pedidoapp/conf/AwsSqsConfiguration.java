bc57551362731c8154775794cf6024c16c7d595b
Thiago <thiagobsn@gmail.com>
2024-05-07 20:46:45 -0300

    Conf sqs

---
+++ src/main/java/com/fiap/pedidoapp/conf/AwsSqsConfiguration.java
@@ -0,0 +1,39 @@
+package com.fiap.pedidoapp.conf;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicSessionCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.sqs.AmazonSQSAsync;
+import com.amazonaws.services.sqs.AmazonSQSAsyncClientBuilder;
+
+@Configuration
+public class AwsSqsConfiguration {
+
+    @Value("${cloud.aws.region.static}")
+    private String region;
+
+    @Value("${cloud.aws.credentials.access-key}")
+    private String awsAccessKey;
+
+    @Value("${cloud.aws.credentials.secret-key}")
+    private String awsSecretKey;
+    
+    @Value("${cloud.aws.credentials.session-token}")
+    private String awsSessionToken;
+
+    @Primary
+    @Bean
+    public AmazonSQSAsync amazonSQSAsync() {
+        return AmazonSQSAsyncClientBuilder.standard().withRegion(Regions.US_EAST_1)
+                .withCredentials(new AWSStaticCredentialsProvider(
+                        new BasicSessionCredentials(awsAccessKey, awsSecretKey, awsSessionToken)))
+                .build();
+
+    }
+
+}
