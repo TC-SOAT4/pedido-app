274a2eebb0a613dbfd5b4ec56900ca437565dcc4
Thiago <thiagobsn@gmail.com>
2024-05-09 20:08:59 -0300

    Ajuste k8s

--- src/main/java/com/fiap/pedidoapp/infrastructure/pedido/schedule/FilaPedidoSchedule.java
+++ src/main/java/com/fiap/pedidoapp/infrastructure/pedido/schedule/FilaPedidoSchedule.java
@@ -2,10 +2,12 @@ package com.fiap.pedidoapp.infrastructure.pedido.schedule;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import lombok.RequiredArgsConstructor;
+import com.fiap.pedidoapp.application.pedido.usecases.EnviarPedidosPagosParaPreparacao;
 
+import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 @Component
@@ -13,14 +15,12 @@ public class FilaPedidoSchedule {
 
     Logger logger = LoggerFactory.getLogger(FilaPedidoSchedule.class);
 
-    // private final EnviarPedidosPagosParaPreparacao enviarPedidosPagosParaPreparacao;
+    private final EnviarPedidosPagosParaPreparacao enviarPedidosPagosParaPreparacao;
 
-    //TODO iniciar envio para fila
-    
-    // @Scheduled(fixedDelay = 5000)
+    @Scheduled(fixedDelay = 5000)
     public void enviarPedidoParaFila() {
         logger.info("Iniciando envio");
-        // enviarPedidosPagosParaPreparacao.enviar();
+        enviarPedidosPagosParaPreparacao.enviar();
     }
 
 }
